d73d923cfacb33093a97e34ffb811b364f14d1aa
rohgr <rdagrawal6@gmail.com>
2022-02-21 18:36:06 +0530

    employee wage User case 1

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,11 +1,15 @@
 
-
-
-
 public class EmployeeWage {
 
 
         public static void main(String[] args) {
-            System.out.print("Welcome to Employee wage program");
+            System.out.println("Welcome to Employee wage program");
+
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        System.out.println(empcheck);
+        if ( empcheck == 1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
         }
 }
