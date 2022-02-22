ce43539c3a66abf27087416493ce8c9fc2760a39
rohgr <rdagrawal6@gmail.com>
2022-02-22 17:09:55 +0530

    Addin Files UC1

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,11 +1,23 @@
-
-
-
-
 public class EmployeeWage {
 
 
         public static void main(String[] args) {
-            System.out.print("Welcome to Employee wage program");
+            System.out.println("Welcome to Employee wage program");
+
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+            if (empCheck == IS_FULL_TIME) {
+                empHrs = 8;
+            }    else
+            {
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Emp wage:  " + empWage);
+
         }
 }
